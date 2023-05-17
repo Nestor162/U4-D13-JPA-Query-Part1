3c6b78cfd806813389b71e973bc85c0af4c899b2
Nestor162 <nestorcicardini@gmail.com>
2023-05-17 14:43:36 +0200

    Aggunte annotations

--- src/main/java/entities/Persona.java
+++ src/main/java/entities/Persona.java
@@ -3,7 +3,19 @@ package entities;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Persona {
+	@Id
 	private UUID id;
 	private String nome;
 	private String cognome;
@@ -15,4 +27,25 @@ public class Persona {
 	public enum Sesso {
 		Maschio, Femmina
 	}
+
+	public Persona(String nome, String cognome, String email,
+			LocalDate dataNascita, Sesso sesso,
+			Partecipazione listaPartecipazioni) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.dataNascita = dataNascita;
+		this.sesso = sesso;
+		this.listaPartecipazioni = listaPartecipazioni;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nome=" + nome + ", cognome=" + cognome
+				+ ", email=" + email + ", dataNascita=" + dataNascita
+				+ ", sesso=" + sesso + ", listaPartecipazioni="
+				+ listaPartecipazioni + "]";
+	}
+
 }
